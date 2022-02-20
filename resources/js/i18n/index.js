c606686b80c884d3c613b91d5b3508532a9ba3ab
Anjianto <anjianto06@gmail.com>
2022-02-20 12:49:10 +0700

    fix: file not found

--- resources/js/i18n/index.js
+++ resources/js/i18n/index.js
@@ -1,23 +1,23 @@
-import Vue from 'vue';
-import VueI18n from 'vue-i18n';
-import en from './lang/en.json';
-import id from './lang/id.json';
-import cn from './lang/cn.json';
-import sk from './lang/sk.json';
+import Vue from "vue";
+import VueI18n from "vue-i18n";
+import en from "./lang/en.json";
+// import id from './lang/id.json';
+import cn from "./lang/cn.json";
+import sk from "./lang/sk.json";
 
 const messages = {
   en,
-  id,
+  // id,
   cn,
-  sk
-}
+  sk,
+};
 
 Vue.use(VueI18n);
 
 const i18n = new VueI18n({
-    locale: config.language,
-    messages,
-    silentTranslationWarn: true,
+  locale: config.language,
+  messages,
+  silentTranslationWarn: true,
 });
 
 export default i18n;
