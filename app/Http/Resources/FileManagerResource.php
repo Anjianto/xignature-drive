3f320cddf6636c7f0afbd40938affd70e0064944
Xignature <xignature-drive@circlecreative.dev>
2022-03-06 12:06:12 +0700

    update resoece get file

--- app/Http/Resources/FileManagerResource.php
+++ app/Http/Resources/FileManagerResource.php
@@ -14,6 +14,24 @@ class FileManagerResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            "user_id" => $this->user_id,
+            "unique_id" => $this->unique_id,
+            "folder_id" => $this->folder_id,
+            "thumbnail" => $this->thumbnail,
+            "name" => $this->name,
+            "basename" => $this->basename,
+            "mimetype" => $this->mimetype,
+            "filesize" => $this->filesize,
+            "type" => $this->type,
+            "metadata" => $this->metadata,
+            'file_url' => $this->file_url,
+            'file_pdf' => $this->file_url ? base64_encode(file_get_contents($this->file_url)): null,
+            "user_scope" => $this->user_scope,
+            "deleted_at" => $this->deleted_at,
+            "created_at" => $this->created_at,
+            "updated_at" => $this->updated_at,
+        ];
     }
 }
